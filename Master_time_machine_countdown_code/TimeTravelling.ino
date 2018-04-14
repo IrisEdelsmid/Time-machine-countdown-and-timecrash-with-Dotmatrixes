bf813ea3f206b9eed9644c5db73f1ad2ae8b6c7c
PimdeJong <38374981+PimdeJong@users.noreply.github.com>
2018-04-14 15:47:11 +0200

    error
    Co-Authored-By: Iris Edelsmid <irisedelsmid@users.noreply.github.com>

--- Master_time_machine_countdown_code/TimeTravelling.ino
+++ Master_time_machine_countdown_code/TimeTravelling.ino
@@ -1,1 +1,22 @@
-
+void error() {
+  bool breakdouwn = false;
+  for (int i = 0; i < totalScreens; i++) {
+    lc1.clearDisplay(i);         /* and clear the display */
+    lc2.clearDisplay(i);         /* and clear the display */
+  }
+  breakdouwn = true;
+  int l, c, r;
+  while (breakdouwn == true) {
+    l =  random(0, 8);
+    c = random (0, 8);
+    r = random (0, 8);
+    lc1.setLed(l, r, c, true);
+    lc2.setLed(l, r, c, true);
+    l =  random(0, 8);
+    c = random (0, 8);
+    r = random (0, 8);
+    lc1.setLed(l, r, c, false);
+    lc2.setLed(l, r, c, false);
+    delay (100);
+  }
+}
